ba4dd667089e031fe1826e3fbdc57ee2700e15cd
Sabina Maritza Moenzil <sabinamoenzil@icloud.com>
2024-04-17 20:18:48 +0700

    Implement update method in Subscriber model to send notification HTTP requests

--- src/model/subscriber.rs
+++ src/model/subscriber.rs
@@ -1,3 +1,5 @@
+use std::ops::Sub;
+
 use rocket::serde::{Deserialize, Serialize};
 use rocket::log;
 use rocket::serde::json::to_string;
@@ -10,4 +12,17 @@ use crate::model::notification::Notification;
 pub struct Subscriber {
     pub url: String,
     pub name: String,
+}
+
+impl Subscriber {
+    #[tokio::main]
+    pub async fn update(&self, payload: Notification) {
+        REQWEST_CLIENT
+            .post(&self.url)
+            .header("Content-Type", "JSON")
+            .body(to_string(&payload).unwrap())
+            .send().await.ok();
+        log::warn_!("Sent {} notification of: [{}] {}, to {}",
+            payload.status, payload.product_type, payload.product_title, self.url);
+    }
 }
