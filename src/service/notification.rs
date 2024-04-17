1396cb116fcffebc5e17abdaaac5fc9aab2b6815
Sabina Maritza Moenzil <sabinamoenzil@icloud.com>
2024-04-17 20:25:09 +0700

    Implement notify function in Notification service to notify each Subscriber

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -2,11 +2,14 @@ use std::ops::Sub;
 use std::thread;
 
 use bambangshop::{Result, compose_error_response};
+use rocket::http::hyper::uri::PathAndQuery;
 use rocket::http::Status;
 use crate::model::notification::Notification;
 use crate::model::product::Product;
 use crate::model::subscriber::{self, Subscriber};
 use crate::repository::subscriber::SubscriberRepository;
+
+use super::product;
 
 pub struct NotificationService;
 
@@ -29,5 +32,23 @@ impl NotificationService {
             ));
         }
         return Ok(result.unwrap());
+    }
+
+    pub fn notify(&self, product_type: &str, status: &str, product: Product) {
+        let mut payload: Notification = Notification {
+            product_title: product.clone().title,
+            product_type: String::from(product_type),
+            product_url: product.clone().get_url(),
+            subscriber_name: String::from(""),
+            status: String::from(status)
+        };
+
+        let subscribers: Vec<Subscriber> = SubscriberRepository::list_all(product_type);
+        for subscriber in subscribers {
+            payload.subscriber_name = subscriber.clone().name;
+            let subscriber_clone = subscriber.clone();
+            let payload_clone = payload.clone();
+            thread::spawn(move || subscriber_clone.update(payload_clone));
+        }
     }
 }
